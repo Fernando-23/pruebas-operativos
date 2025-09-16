dc387d3ad80ff16ee13451a2a42ab9da01a1878b
Fer <faquinoaltamirano@frba.utn.edu.ar>
2025-09-16 03:19:32 +0000

    listas del super

---
+++ listas/listas-pruebas.c
@@ -0,0 +1,44 @@
+#include "listas-pruebas.h"
+
+
+int quid_global = 0;
+t_list* lista_ready; 
+
+void crearQCBMock(Qcb* qcb,char* query){
+    qcb->query = query;
+    qcb->quid = quid_global;
+
+    //wait
+    quid_global++;
+    //signal
+    list_add(lista_ready, qcb);
+    //printf("Debug - (CrearQCB) - Se creo el QCB con Quid: %d y Query: %s", qcb->quid, qcb->query);
+
+} 
+
+void mostrarLista(void* qcb_void){
+    Qcb* qcb_a_mostrar = (Qcb *)qcb_void;
+    printf("QCB - id:%d, query:%s \n",qcb_a_mostrar->quid,qcb_a_mostrar->query);
+}
+
+
+int main (){
+
+    lista_ready = list_create();
+    Qcb* qcb_1 = malloc(sizeof(Qcb));
+    Qcb* qcb_2 = malloc(sizeof(Qcb));
+    Qcb* qcb_3 = malloc(sizeof(Qcb));
+    Qcb* qcb_4 = malloc(sizeof(Qcb));
+    Qcb* qcb_5 = malloc(sizeof(Qcb));
+
+    crearQCBMock(qcb_1,"santi");
+    crearQCBMock(qcb_2,"liam");
+    crearQCBMock(qcb_3,"fer");
+    crearQCBMock(qcb_4,"agus");
+    crearQCBMock(qcb_5,"abi");
+
+    list_iterate(lista_ready, mostrarLista);
+  
+    list_destroy(lista_ready);
+
+}
